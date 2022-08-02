88d2395f74e1545555177540f6ad4348c9db4dbe
khalilannbiya <syeichkhalil@gmail.com>
2022-08-02 17:22:02 +0700

    feat: create init emojis component

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Container from "./Container";
 import Empty from "./Empty";
+import Emojis from "./Emojis";
 
-export { Navbar, Container, Empty };
+export { Navbar, Container, Empty, Emojis };
